1284b41ef4f3f6c1689bb0c613086a4329cd7e4f
adreaskar <adrian26mccous@gmail.com>
2024-06-13 22:56:42 +0300

    Created 404 page

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div className="w-full text-center p-60 text-4xl">
+            <h1>404 | Page Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
