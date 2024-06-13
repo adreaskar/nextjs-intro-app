f3f40e92f7ef274b3a2095c1d6680e022e93a1f0
adreaskar <adrian26mccous@gmail.com>
2024-06-13 22:57:02 +0300

    Created home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <main className="items-center py-14 w-1/2 m-auto">
+            <h1 className="text-6xl font-bold">The best todo app out there.</h1>
+            <p className="text-lg mt-5 opacity-50">
+                Recommended by all minimalists.
+            </p>
+
+            <button className="duration-200 mt-16 py-3 px-5 rounded-lg border border-neutral-700 hover:bg-neutral-800/30">
+                <Link href="/todos">Create a todo -&gt;</Link>
+            </button>
+        </main>
+    );
 }
