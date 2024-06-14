fb732d7e76d9957b3b9807a8d76f413ef0d1f59a
adreaskar <adrian26mccous@gmail.com>
2024-06-14 19:52:04 +0300

    Updated TodoItem component

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,8 +1,21 @@
+'use client';
+
+import { FiCircle, FiCheckCircle } from 'react-icons/fi';
+import { checkTodo } from '@/utils/actions';
+
 const TodoItem = ({ todo }) => {
     return (
-        <div className="flex justify-between items-center border-b border-neutral-700 py-2">
-            <p>{todo.content}</p>
-            <p>{todo.due}</p>
+        <div
+            onClick={() => checkTodo(todo.id)}
+            className={`mt-2 flex justify-between items-center p-2 hover:cursor-pointer hover:bg-neutral-700/30 rounded-md ${
+                todo.completed && 'text-neutral-500'
+            }`}
+        >
+            <div className="flex items-center gap-2">
+                {todo.completed ? <FiCheckCircle /> : <FiCircle />}
+                <p>{todo.content}</p>
+            </div>
+            <p>{todo.dueDate}</p>
         </div>
     );
 };
