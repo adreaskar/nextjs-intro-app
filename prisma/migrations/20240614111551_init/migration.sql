f978982a5ca5bb799100bd6e7609f30da9aa56cd
adreaskar <adrian26mccous@gmail.com>
2024-06-14 14:59:04 +0300

    Modified prisma configs and migrated database

---
+++ prisma/migrations/20240614111551_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "content" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false
+);
