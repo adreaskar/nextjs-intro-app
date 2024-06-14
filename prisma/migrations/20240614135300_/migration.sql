310f94f5a10a12f0405dbaacbd6b959bd379ad55
adreaskar <adrian26mccous@gmail.com>
2024-06-14 19:49:32 +0300

    Updated db migration

---
+++ prisma/migrations/20240614135300_/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `dueDate` to the `Todo` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "content" TEXT NOT NULL,
+    "dueDate" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Todo" ("completed", "content", "createdAt", "id", "updatedAt") SELECT "completed", "content", "createdAt", "id", "updatedAt" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
